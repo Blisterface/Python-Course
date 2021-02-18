caf31fb8e654e8b72b41378f1583051079289bad
Joseph Mhlongo <joseph.mhlongo@Joseph-Mhlongo.local>
2021-02-18 22:51:09 +0200

    lists and dictionary

---
+++ exercises/lists.py
@@ -0,0 +1,12 @@
+friends = [{"Joseph":32}, {"Mark":30}, {"Danny":29}]
+
+print(friends[0])
+
+people = [
+    ["Rose",24],
+    ["James",30],
+    ["Anna",28],
+    ["Joe",56]
+]
+
+print(people[2])
